161f52c8b14284839ca8cbdd0369a0a8573466df
Brendan O'Malley <bromal23@live.unc.edu>
2022-04-24 18:31:06 -0400

    fix comments

--- public/main.js
+++ public/main.js
@@ -1,11 +1,5 @@
+
 // Focus div based on nav button click
-
-// Flip one coin and show coin image to match result when button clicked
-
-// Flip multiple coins and show coin images in table as well as summary results
-// Enter number and press button to activate coin flip series
-
-// Guess a flip by clicking either heads or tails button
 document.getElementById("homenav").onclick = function(){
     document. location. reload();
     document.getElementById("homenav").className = "active";
